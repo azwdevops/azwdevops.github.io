9503f044b20c8de9ad4e4daa61e8929042e12d29
Zachary Waweru <azwdevops@gmail.com>
2024-04-13 12:49:24 +0300

    added display for current stage in breathing timer

--- breathing-timer/main.js
+++ breathing-timer/main.js
@@ -8,6 +8,7 @@ const minutesInput = document.getElementById("minutesInput");
 const secondsInput = document.getElementById("secondsInput");
 const startButton = document.getElementById("startButton");
 const timerDisplay = document.getElementById("timerDisplay");
+const stage = document.getElementById("stage");
 
 startButton.addEventListener("click", () => {
   const hours = parseInt(hoursInput.value, 10) || 0;
@@ -28,6 +29,15 @@ function startTimer() {
 }
 
 function runLoop() {
+  if (loopCount === 0) {
+    stage.textContent = "Breathe in.....";
+  } else if (loopCount === 1) {
+    stage.textContent = "Hold breathe.....";
+  } else if (loopCount === 2) {
+    stage.textContent = "Breathe out.....";
+  } else {
+    stage.textContent = "";
+  }
   if (remainingTime >= 0) {
     const hours = Math.floor(remainingTime / 3600);
     const minutes = Math.floor((remainingTime % 3600) / 60);
